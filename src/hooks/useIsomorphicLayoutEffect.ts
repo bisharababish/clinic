dc5db0880532e7a5d51cf5af30a7f7ac56991550
bisharababish <62569803+bisharababish@users.noreply.github.com>
2025-06-23 14:50:58 +0300

    s

--- src/hooks/useIsomorphicLayoutEffect.ts
+++ src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,9 +1,16 @@
 // hooks/useIsomorphicLayoutEffect.js
 import { useEffect, useLayoutEffect } from "react";
 
-const useIsomorphicLayoutEffect =
-    typeof window !== "undefined" && typeof useLayoutEffect !== "undefined"
-        ? useLayoutEffect
-        : useEffect;
+// Always use useEffect during SSR/static export to avoid React warnings
+const isDOM = typeof window !== "undefined" && typeof document !== "undefined";
+
+const useIsomorphicLayoutEffect = isDOM ? useLayoutEffect : useEffect;
+
+if (process.env.NODE_ENV === "development") {
+    // eslint-disable-next-line no-console
+    console.log(
+        `[useIsomorphicLayoutEffect] Using: ${isDOM ? "useLayoutEffect" : "useEffect"}`
+    );
+}
 
 export default useIsomorphicLayoutEffect;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import './ssr-polyfill.js';
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
